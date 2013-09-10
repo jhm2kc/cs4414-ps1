09ee4bae4ab714740f6e1b112a99009cdfb6afe0
UVa student <student@student-VirtualBox.(none)>
2013-09-10 00:27:53 -0400

    finished code and answers

--- zhttpto.rs
+++ zhttpto.rs
@@ -14,6 +14,7 @@ use std::str;
 static BACKLOG: uint = 5;
 static PORT:    uint = 4414;
 static IPV4_LOOPBACK: &'static str = "127.0.0.1";
+static mut visitor_count: int = 0;
 
 fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::comm::SharedChan<Option<extra::net_tcp::TcpErrData>>)
 {
@@ -24,18 +25,24 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
                println(fmt!("Connection error: %?", err));
             },  
             Ok(sock) => {
+
                 let peer_addr: ~str = net_ip::format_addr(&sock.get_peer_addr());
                 println(fmt!("Received connection from: %s", peer_addr));
-                
+
                 let read_result = net_tcp::read(&sock, 0u);
                 match read_result {
                     Err(err) => {
                         println(fmt!("Receive error: %?", err));
                     },
                     Ok(bytes) => {
+		unsafe {visitor_count +=1;
+		println(fmt!("visitor count: %d",visitor_count));}
                         let request_str = str::from_bytes(bytes.slice(0, bytes.len() - 1));
                         println(fmt!("Request received:\n%s", request_str));
-                        let response: ~str = ~
+let end_path = request_str.find_str("HTTP").get();
+		let path = request_str.slice_chars(5,end_path).trim();
+		if path.is_empty() {
+let response: ~str = ~
                             "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
                              <doctype !html><html><head><title>Hello, Rust!</title>
                              <style>body { background-color: #111; color: #FFEEAA }
@@ -44,8 +51,25 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
                              <body>
                              <h1>Greetings, Rusty!</h1>
                              </body></html>\r\n";
-
                         net_tcp::write(&sock, response.as_bytes_with_null_consume());
+}
+	else {
+
+		let file_path = ~std::path::PosixPath(path);
+		match std::io::read_whole_file_str(file_path) {
+    			Ok(file_data) => {
+
+			let response: ~str = file_data;
+                        net_tcp::write(&sock, response.as_bytes_with_null_consume());
+                       
+
+			}
+   			 Err(err) => {
+       				 println(err);
+    			}
+		}
+	}
+
                     },
                 };
             }
